c9fe736c388f445f900afdd6dd3f481a564021c1
Aashritha2810 <aashritha.tiru@gmail.com>
2025-02-15 10:16:44 +0530

    Add files via upload

--- Divison.java
+++ Divison.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 public class Divison{
 	public static void main(String args[]){
-	float a= 32;
-	float b=9;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the numerator");
+	float a= sc.nextFloat();
+    System.out.println("Enter the Denominator");
+	float b=sc.nextFloat();
 	float c= 0;
 	c= a / b;
 	System.out.println(c);
